155b7214b703aa6acdc0290641a759c2a24d5e05
Arkadii <lviv.home31@gmail.com>
2018-03-03 00:32:05 +0200

    First commit
    Soooo......

---
+++ IpWebCam/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace IpWebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.PictureBox();
+            this.disButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.outputBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(38, 12);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(125, 20);
+            this.ipTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(169, 10);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputBox.Location = new System.Drawing.Point(15, 38);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(519, 357);
+            this.outputBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.outputBox.TabIndex = 3;
+            this.outputBox.TabStop = false;
+            // 
+            // disButton
+            // 
+            this.disButton.Location = new System.Drawing.Point(459, 10);
+            this.disButton.Name = "disButton";
+            this.disButton.Size = new System.Drawing.Size(75, 23);
+            this.disButton.TabIndex = 4;
+            this.disButton.Text = "Disconnect";
+            this.disButton.UseVisualStyleBackColor = true;
+            this.disButton.Click += new System.EventHandler(this.disButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(66, 446);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 481);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.disButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ipTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.outputBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.PictureBox outputBox;
+        private System.Windows.Forms.Button disButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ IpWebCam/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MjpegProcessor;
+namespace IpWebCam
+{
+    public partial class Form1 : Form
+    {
+        MjpegDecoder mjpeg;
+        String ipAddress;
+        SoundPlayer sound;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            mjpeg = new MjpegDecoder();
+            mjpeg.FrameReady += frameReady;
+        }
+
+        private void frameReady(object sender, FrameReadyEventArgs e)
+        {
+            outputBox.Image = e.Bitmap;
+            
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            ipAddress = "http://" + ipTextBox.Text.ToString();
+            mjpeg.ParseStream(new Uri(ipAddress + "/videofeed"));
+            sound = new SoundPlayer();
+            
+        }
+
+        private void disButton_Click(object sender, EventArgs e)
+        {
+            mjpeg.StopStream();
+        }
+
+        private void button1_Click  (object sender, EventArgs e)
+        {
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(ipAddress + "/enabletorch");
+            wr.GetResponse().Close();
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(ipAddress + "/disabletorch");
+            wr.GetResponse().Close();
+        }
+    }
+}
